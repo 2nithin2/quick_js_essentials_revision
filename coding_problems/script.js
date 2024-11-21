3d2258da1c9b7a0b07c86792d52319daf4b5041a
2nithin2 <official.nithinraj@gmail.com>
2024-11-21 20:41:42 +0530

    color picker excercise

---
+++ coding_problems/script.js
@@ -0,0 +1,25 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function clickButton1() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+
+}
+
+function clickButton2() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+
+}
+
+function clickButton3() {
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+}
+
+function clickButton4() {
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+
+}
